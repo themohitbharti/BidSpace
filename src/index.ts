1234a95b1a01015c1e233956115fe7a013429a54
Mohit Bharti <mohitbharti373@gmail.com>
2024-07-30 18:34:43 +0530

    connected web-socket

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 import dotenv from "dotenv";
 import express from "express";
+import { createServer } from 'http';
+import { Server } from 'socket.io';
 import { app } from "./app";
 import connectDB from "./db/conn";
 
@@ -8,11 +10,34 @@ connectDB();
 
 const port = process.env.PORT;
 
+const server = createServer(app);
+
+const io = new Server(server, {
+  cors: {
+    origin: process.env.CORS_ORIGIN,
+    methods: ["GET", "POST"]
+  }
+});
+
+io.on("connection" , (socket) => {
+  console.log(`user connected ${socket.id}`)
+
+  socket.on("message" , (message) => {
+      console.log("message: ", message)
+
+      io.emit("message" , message)
+  })
+
+  socket.on("disconnect" ,() => {
+      console.log(`${socket.id} disconnected`)
+  })
+})
+
 app.get("/", (req: any, res: any) => {
   res.send("nazro me ho tum meri");
   console.log("khona chahu mai raat bhar");
 });
 
-app.listen(port, () => {
+server.listen(port, () => {
   console.log(`Server is up and Running on port ${port}`);
 });
