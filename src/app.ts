2a005665c6ee8ed0efe033a7d65defbff3dfb309
Mohit Bharti <mohitbharti373@gmail.com>
2025-04-16 23:28:02 +0530

    refactor CORS configuration and update cookie options for user authentication

--- src/app.ts
+++ src/app.ts
@@ -23,12 +23,29 @@ const limiter = rateLimit({
   max: 2500, // limit each IP to 2500 requests per windowMs
 });
 
-app.use(
-  cors({
-    origin: process.env.CORS_ORIGIN,
-    credentials: true,
-  })
-);
+// app.use(
+//   cors({
+//     origin: process.env.CORS_ORIGIN,
+//     credentials: true,
+//   })
+// );
+
+const allowedOrigins = [
+  "http://localhost:3000",  // frontend dev
+  "https://bidspace-v1.com" // deployed frontend (when ready)
+];
+
+app.use(cors({
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  credentials: true,
+}));
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
