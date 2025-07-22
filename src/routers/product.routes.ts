515510c1d793cff8568c3fcccc7fafd6e56e0107
Mohit Bharti <mohitbharti373@gmail.com>
2025-07-22 09:55:45 +0530

    refactor product routes to remove token verification from showByCategory endpoint

--- src/routers/product.routes.ts
+++ src/routers/product.routes.ts
@@ -29,7 +29,7 @@ router.post(
 
 router.get("/waiting", verifyToken, showWaitingPurchases);
 
-router.get("/list/:category/:status", verifyToken, showByCategory);
+router.get("/list/:category/:status", showByCategory);
 
 router.get("/details/:id", showProductDetails);
 
